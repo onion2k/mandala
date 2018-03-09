fc8d1d780e2957319a45c98d4a0258d73ed3e3eb
Chris Neale <chris@komododigital.co.uk>
2018-03-09 11:24:36 +0000

    First version working

---
+++ src/index.js
@@ -0,0 +1,95 @@
+import './mandala.scss';
+
+let primitives;
+Math.TAU = Math.PI * 2;
+
+function init() {
+
+    primitives = {
+        width: document.body.clientWidth,
+        height: document.body.clientHeight,
+        center: {
+            x: document.body.clientWidth / 2,
+            y: document.body.clientHeight / 2
+        },
+        radius: Math.min(document.body.clientWidth, document.body.clientHeight) * 0.5,
+        segment: 36,
+    }
+    
+    let canvas = document.createElement('canvas');
+    canvas.width = primitives.width;
+    canvas.height = primitives.height;
+
+    document.body.appendChild(canvas);
+
+    render();
+
+}
+
+function segment() {
+
+    let p = primitives;
+
+    let segmentCanvas = document.createElement('canvas');
+    segmentCanvas.width  = 200;
+    segmentCanvas.height = p.height;
+
+    let segmentCanvasCtx = segmentCanvas.getContext('2d');
+
+    segmentCanvasCtx.strokeStyle = 'red';
+    segmentCanvasCtx.beginPath();
+    segmentCanvasCtx.moveTo(100, 0);
+    segmentCanvasCtx.quadraticCurveTo(100, p.center.y / 3, 0, p.center.y);
+    segmentCanvasCtx.stroke();
+
+    segmentCanvasCtx.beginPath();
+    segmentCanvasCtx.moveTo(100, 0);
+    segmentCanvasCtx.quadraticCurveTo(100, p.center.y / 3, 200, p.center.y);
+    segmentCanvasCtx.stroke();
+
+    segmentCanvasCtx.beginPath();
+    segmentCanvasCtx.arc(100, p.center.y/2, 5, 0, Math.PI*2);
+    segmentCanvasCtx.stroke();
+
+    return segmentCanvas;
+
+}
+
+function render(){
+
+    let seg = segment();
+
+    let p = primitives;
+    let ctx = document.querySelector('canvas').getContext('2d');
+    let s = (Math.TAU / p.segment) / 2;
+
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0,0,p.width,p.height);
+
+    for (let x=0; x < p.segment; x++) {
+
+        ctx.translate(p.center.x, p.center.y);
+
+        ctx.rotate(s*2);
+
+        // ctx.fillStyle = '#222222';
+        // ctx.beginPath();
+        // ctx.moveTo(0, 0);
+        // ctx.lineTo(Math.cos(-s + Math.PI/2) * p.radius, Math.sin(-s + Math.PI/2) * p.radius);
+        // ctx.lineTo(Math.cos(s + Math.PI/2) * p.radius, Math.sin(s + Math.PI/2) * p.radius);
+        // ctx.closePath();
+        // ctx.fill();
+
+        ctx.drawImage(seg, -seg.width/2, 0);
+
+        ctx.translate(-p.center.x,-p.center.y);
+
+    }
+
+    ctx.fillStyle = 'white';
+    ctx.beginPath();
+    ctx.arc(p.center.x,p.center.y, 3, 0, Math.PI*2);
+    ctx.fill();
+}
+
+document.addEventListener('DOMContentLoaded', init);
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+  const path = require('path');
+  const HtmlWebpackPlugin = require('html-webpack-plugin');
+  const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+  module.exports = {
+    entry: {
+      app: './src/index.js'
+    },
+    mode: 'development',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [{
+                loader: "style-loader" // creates style nodes from JS strings
+            }, {
+                loader: "css-loader" // translates CSS into CommonJS
+            }, {
+                loader: "sass-loader" // compiles Sass to CSS
+            }]
+        }]
+    },
+    plugins: [
+      new CleanWebpackPlugin(['dist']),
+      new HtmlWebpackPlugin({
+        title: 'Development'
+      })
+    ],
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    }
+  };
