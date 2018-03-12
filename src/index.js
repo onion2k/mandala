1768c4db5151ae8a95c159977f55e18326380ee2
Chris Neale <chris@usablehq.com>
2018-03-12 19:40:44 +0000

    Started filled petals

--- src/index.js
+++ src/index.js
@@ -1,48 +1,51 @@
 import './mandala.css';
 
 let primitives;
+let counter = 0;
+
 Math.TAU = Math.PI * 2;
 
-function init() {
+function widthAtHeight(h){
 
-    primitives = {
-        width: document.body.clientWidth,
-        height: document.body.clientHeight,
-        center: {
-            x: document.body.clientWidth * 0.5,
-            y: document.body.clientHeight * 0.5
-        },
-        radius: Math.min(document.body.clientWidth, document.body.clientHeight) * 0.5,
-        segment: 16,
-    };
+  let p = primitives;
 
-    primitives._width = (2 * primitives.radius) * Math.tan((180 / primitives.segment) * (Math.PI / 180)) + 2;
-    primitives.middle = primitives._width * 0.5;
-
-    let canvas = document.createElement('canvas');
-    canvas.width = primitives.width;
-    canvas.height = primitives.height;
-
-    document.body.appendChild(canvas);
-
-    render();
+  return (2 * h) * Math.tan((180 / p.segment) * (Math.PI / 180));
 
 }
 
-function petal(ctx, pos, size, color) {
+function fill(c, pos, color) {
+
+    let p = primitives;
+    
+    let wah = widthAtHeight(pos);
+
+    c.fillStyle = color;
+    c.beginPath();
+    c.moveTo(p.middle - (wah/2) - 5, pos);
+    c.lineTo(p.middle + (wah/2) + 5, pos);
+    c.lineTo(p.middle, 0);
+    c.closePath();
+    c.fill();
+
+}
+
+function petal(c, pos, size, color, stroke) {
 
     let p = primitives;
 
-    ctx.fillStyle = color;
-    ctx.beginPath();
-    ctx.moveTo(p.middle, pos);
-    ctx.quadraticCurveTo(p.middle - size, pos, p.middle, (pos + size));
-    ctx.quadraticCurveTo(p.middle + size, pos, p.middle, pos);
-    ctx.fill();
+    c.fillStyle = color;
+    c.beginPath();
+    c.moveTo(p.middle, pos);
+    c.quadraticCurveTo(p.middle - size, pos, p.middle, (pos + size));
+    c.quadraticCurveTo(p.middle + size, pos, p.middle, pos);
+    c.fill();
+    
+    if (stroke) {
+      c.strokeStyle = stroke;
+      c.stroke();
+    }
 
 }
-
-let counter = 0;
 
 function segment() {
 
@@ -67,79 +70,47 @@ function segment() {
     // segmentCanvasCtx.stroke();
     segmentCanvasCtx.clip();
 
-    segmentCanvasCtx.fillStyle = 'white';
-    segmentCanvasCtx.beginPath();
-    segmentCanvasCtx.moveTo(p.middle, p.center.y * 0.25);
-    segmentCanvasCtx.quadraticCurveTo(0, p.center.y * 0.5, p.middle, p.center.y * 0.75);
-    segmentCanvasCtx.quadraticCurveTo(p._width, p.center.y * 0.5, p.middle, p.center.y * 0.25);
-    segmentCanvasCtx.fill();
+    petal(segmentCanvasCtx, p.center.y * 0.55, widthAtHeight(p.center.y * 0.6), 'turquoise', 'white');
+    fill(segmentCanvasCtx, p.center.y * 0.6, 'turquoise');
 
-    petal(segmentCanvasCtx, x, -100, 'yellow');
+    petal(segmentCanvasCtx, p.center.y * 0.33, widthAtHeight(p.center.y * 0.35), 'black', 'black');
+    fill(segmentCanvasCtx, p.center.y * 0.35, 'black');
 
-    // segmentCanvasCtx.fillStyle = 'yellow';
-    // segmentCanvasCtx.beginPath();
-    // segmentCanvasCtx.moveTo(p.middle, p.center.y * 0.5);
-    // segmentCanvasCtx.quadraticCurveTo(p.middle - 50, p.center.y * 0.75, p.middle, p.center.y * 0.75);
-    // segmentCanvasCtx.quadraticCurveTo(p.middle + 50, p.center.y * 0.75, p.middle, p.center.y * 0.5);
-    // segmentCanvasCtx.fill();
+    petal(segmentCanvasCtx, p.center.y * 0.25, widthAtHeight(p.center.y * 0.25) - 10, 'yellow');
+    petal(segmentCanvasCtx, p.center.y * 0.5 + 10, widthAtHeight(p.center.y * 0.5) - 10, 'blue');
 
-    segmentCanvasCtx.fillStyle = 'blue';
+    segmentCanvasCtx.fillStyle = 'red';
     segmentCanvasCtx.beginPath();
     segmentCanvasCtx.moveTo(0, p.center.y * 0.75);
-    segmentCanvasCtx.quadraticCurveTo(p._width/2, p.center.y * 0.75, 0, p.center.y);
+    segmentCanvasCtx.quadraticCurveTo(p._width * 0.5, p.center.y * 0.75, 0, p.center.y * 0.9);
     segmentCanvasCtx.fill();
 
     segmentCanvasCtx.beginPath();
     segmentCanvasCtx.moveTo(p._width, p.center.y * 0.75);
-    segmentCanvasCtx.quadraticCurveTo(p._width/2, p.center.y * 0.75, p._width, p.center.y);
+    segmentCanvasCtx.quadraticCurveTo(p._width * 0.5, p.center.y * 0.75, p._width, p.center.y * 0.9);
     segmentCanvasCtx.fill();
 
-    segmentCanvasCtx.fillStyle = 'orange';
+    segmentCanvasCtx.fillStyle = 'white';
     segmentCanvasCtx.beginPath();
-    segmentCanvasCtx.moveTo(p._width/2, p.center.y * 0.75);
-    segmentCanvasCtx.quadraticCurveTo(p._width/2 - 50, p.center.y * 0.75, p._width/2, p.center.y);
-    segmentCanvasCtx.quadraticCurveTo(p._width/2 + 50, p.center.y * 0.75, p._width/2, p.center.y * 0.75);
+    segmentCanvasCtx.arc(p.middle, p.center.y * 0.8, 3, 0, Math.PI*2);
     segmentCanvasCtx.fill();
 
-    segmentCanvasCtx.strokeStyle = 'white';
-    segmentCanvasCtx.lineWidth = 2;
-    segmentCanvasCtx.fillStyle = 'red';
+    segmentCanvasCtx.fillStyle = 'white';
     segmentCanvasCtx.beginPath();
-    segmentCanvasCtx.moveTo(p.middle, p.center.y * 0.3);
-    segmentCanvasCtx.quadraticCurveTo(p.middle + 20, p.center.y * 0.3, p.middle, p.center.y * 0.2);
-    segmentCanvasCtx.quadraticCurveTo(p.middle - 20, p.center.y * 0.3, p.middle, p.center.y * 0.3);
-    segmentCanvasCtx.fill();
-    segmentCanvasCtx.stroke();
-
-    segmentCanvasCtx.strokeStyle = 'white';
-    segmentCanvasCtx.lineWidth = 1;
-    segmentCanvasCtx.beginPath();
-    segmentCanvasCtx.moveTo(0,p.center.y * 0.5 - 20);
-    segmentCanvasCtx.lineTo(p._width,p.center.y * 0.5 -20);
-    segmentCanvasCtx.stroke();
-
-    segmentCanvasCtx.fillStyle = 'cyan';
-    segmentCanvasCtx.beginPath();
-    segmentCanvasCtx.arc(p._width * 0.25, p.center.y - 15, 3, 0, Math.PI*2);
+    segmentCanvasCtx.arc(p.middle - (widthAtHeight(p.center.y * 0.9) * 0.25), p.center.y * 0.9, 3, 0, Math.PI*2);
     segmentCanvasCtx.fill();
 
-    segmentCanvasCtx.fillStyle = 'cyan';
+    segmentCanvasCtx.fillStyle = 'white';
     segmentCanvasCtx.beginPath();
-    segmentCanvasCtx.arc(p._width * 0.75, p.center.y - 15, 3, 0, Math.PI*2);
+    segmentCanvasCtx.arc(p.middle + (widthAtHeight(p.center.y * 0.9) * 0.25), p.center.y * 0.9, 3, 0, Math.PI*2);
     segmentCanvasCtx.fill();
 
-    let widthAtHeight = (2 * (p.center.y * 0.75)) * Math.tan((180 / primitives.segment) * (Math.PI / 180));
-    let dotx = (p.middle - widthAtHeight/2) + (counter % widthAtHeight);
-    segmentCanvasCtx.fillStyle = 'red';
-    segmentCanvasCtx.beginPath();
-    segmentCanvasCtx.arc(dotx, p.center.y * 0.75, 5, 0, Math.PI*2);
-    segmentCanvasCtx.fill();
 
     return segmentCanvas;
 
 }
 
-function render(){
+function render() {
 
     let seg = segment();
 
@@ -147,7 +118,6 @@ function render(){
     let ctx = document.querySelector('canvas').getContext('2d');
     let s = (Math.TAU / p.segment) * 0.5;
 
-    ctx.strokeStyle = 'white';
     ctx.fillStyle = 'black';
     ctx.fillRect(0,0,p.width,p.height);
 
@@ -156,17 +126,44 @@ function render(){
         ctx.translate(p.center.x, p.center.y);
         ctx.rotate(s*2);
         ctx.drawImage(seg, -seg.width * 0.5, 0);
-        ctx.translate(-p.center.x,-p.center.y);
+        ctx.translate(-p.center.x, -p.center.y);
 
     }
 
     ctx.fillStyle = 'white';
     ctx.beginPath();
-    ctx.arc(p.center.x,p.center.y, 3, 0, Math.PI*2);
+    ctx.arc(p.center.x, p.center.y , 3, 0, Math.PI*2);
     ctx.fill();
 
     requestAnimationFrame(render);
 
 }
+
+function init() {
+
+  primitives = {
+      width: document.body.clientWidth,
+      height: document.body.clientHeight,
+      center: {
+          x: document.body.clientWidth * 0.5,
+          y: document.body.clientHeight * 0.5
+      },
+      radius: Math.min(document.body.clientWidth, document.body.clientHeight) * 0.5,
+      segment: 16,
+  };
+
+  primitives._width = (2 * primitives.radius) * Math.tan((180 / primitives.segment) * (Math.PI / 180)) + 2;
+  primitives.middle = primitives._width * 0.5;
+
+  let canvas = document.createElement('canvas');
+  canvas.width = primitives.width;
+  canvas.height = primitives.height;
+
+  document.body.appendChild(canvas);
+
+  render();
+
+}
+
 
 document.addEventListener('DOMContentLoaded', init);
